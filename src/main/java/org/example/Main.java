c9010c8769d6e7e2998ff2733976c44991e94fa3
Alyona4433 <alena4433@gmail.com>
2023-08-29 14:22:42 +0300

    Added window Game (1st line)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import static org.example.WelcomeWindow.showGameWindow;
 import static org.example.WelcomeWindow.welcomeWindow;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
@@ -7,5 +8,6 @@ import static org.example.WelcomeWindow.welcomeWindow;
 public class Main {
     public static void main(String[] args) {
         welcomeWindow();
+
     }
 }
--- src/main/java/org/example/WelcomeWindow.java
+++ src/main/java/org/example/WelcomeWindow.java
@@ -2,6 +2,8 @@ package org.example;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class WelcomeWindow {
     public static void welcomeWindow() {
@@ -14,8 +16,14 @@ public class WelcomeWindow {
             JLabel welcomeLabel = new JLabel("Welcome to Cities Game! Press Start to begin.");
             JButton startButton = new JButton("Start");
 
-            startButton.addActionListener(e -> {
-                welcomeFrame.setVisible(false);
+            startButton.addActionListener(new ActionListener() {
+
+                @Override
+                        public void actionPerformed(ActionEvent e) {
+                    welcomeFrame.setVisible(false); //Приховання вікна WelcomeWindow
+                    showGameWindow();
+                }
+
 
             });
 
@@ -24,5 +32,25 @@ public class WelcomeWindow {
             welcomeFrame.setLocationRelativeTo(null);
             welcomeFrame.setVisible(true);
         });
+    }
+
+    public static void showGameWindow() {
+        JFrame gameFrame = new JFrame("Cities Game");
+        gameFrame.setSize(800, 600);
+        gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gameFrame.setLayout(new BorderLayout());
+
+        JTextField userInputField = new JTextField(20);
+        JButton submitButton = new JButton("Make a move");
+
+        JPanel inputPanel = new JPanel();
+        inputPanel.add(new JLabel("Enter city name: "));
+        inputPanel.add(userInputField);
+        inputPanel.add(submitButton);
+
+        gameFrame.add(inputPanel, BorderLayout.NORTH);
+
+        gameFrame.setLocationRelativeTo(null);
+        gameFrame.setVisible(true);
     }
 }
